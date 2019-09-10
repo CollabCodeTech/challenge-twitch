b66a9409f83d61949ce5e4b59ee7bf9fb5bb93de
Helton Fábio <heltonfabio@outlook.com>
2019-09-10 02:45:04 -0300

    visor section created

---
+++ 2019-setembro/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Screen from "./containers/Screen.svelte";
+  export let name = "World";
+</script>
+
+<style>
+  h1 {
+    background-color: purple;
+    font-size: 36px;
+    font-weight: bold;
+  }
+</style>
+
+<Screen />
