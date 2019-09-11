4a46cdc2e6e4c5b0d1735cf7a290047ba8c785b9
unknown <ea0123450@gmail.com>
2019-09-11 15:03:55 -0300

    eslint, prettier and editor config

--- 2019-setembro/EduardoAraujoB/src/App.js
+++ 2019-setembro/EduardoAraujoB/src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Começando Projeto</h1>
-      </header>
+        <h1> Começando Projeto </h1>{' '}
+      </header>{' '}
     </div>
   );
 }
